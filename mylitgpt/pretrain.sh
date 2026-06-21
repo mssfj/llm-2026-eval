0020c995b422b57fcd8352ec4bd61ff36f8e4588
Masashi Fujimoto <mss.fujimoto@gmail.com>
2026-06-21 01:18:40 +0000

    pretrain.sh update

--- mylitgpt/pretrain.sh
+++ mylitgpt/pretrain.sh
@@ -1,7 +1,8 @@
 # export HF_DATASET_REPO_ID="username/fineweb-edu-10bt-qwen25"
 # export HF_MODEL_REPO_ID="username/qwen25-0.5b-fineweb-edu-10bt"
 
-huggingface-cli login
+# huggingface-cli login
+hf auth login
 wandb login
 
 cd /root/lowbit-math-reasoning/mylitgpt
